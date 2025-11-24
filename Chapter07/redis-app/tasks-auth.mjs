565c249db8d81cd7ddaad614048b48b951dbc50d
Jerzy Supryn (Gesup) <jerzy.supryn.dev@gmail.com>
2025-11-24 20:55:44 +0100

    added redis auth

---
+++ Chapter07/redis-app/tasks-auth.mjs
@@ -0,0 +1,36 @@
+import "dotenv/config";
+import { createClient } from "redis";
+const client = createClient({port: process.env.PORT, password: process.env.PASSWORD});
+
+const task = process.argv[2];
+client.on("error", (err) => {
+  console.log("Error:", err);
+});
+
+await client.connect();
+
+if (!task) {
+  listTasks();
+} else {
+  addTask(task);
+}
+
+async function addTask(task) {
+  const key = `Task:${Math.random().toString(32).replace(".", "")}`;
+
+  await client.hSet(key, "task", task);
+
+  listTasks();
+}
+
+async function listTasks() {
+  const keys = await client.keys("Task:*");
+
+  for (const key of keys) {
+    const task = await client.hGetAll(key);
+
+    console.log(task);
+  }
+
+  client.quit();
+}
